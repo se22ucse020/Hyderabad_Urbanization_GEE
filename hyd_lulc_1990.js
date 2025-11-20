9f3499130effb9e5745a37fbdfa71a2ed2e7241b
se22ucse020 <bunnymadhu05@gmail.com>
2025-11-20 16:38:04 +0530

    Update hyd_lulc_1990.js

--- hyd_lulc_1990.js
+++ hyd_lulc_1990.js
@@ -14,3 +14,68 @@ var visL5 = {bands: ['SR_B3','SR_B2','SR_B1'], min: 7000, max: 12000};
 Map.addLayer(l5.clip(hyderabad), visL5, 'Landsat 5 - 1990');
 
 // --- TODO: Add classification next ---
+// ================== CLASSIFICATION BLOCK ==================
+
+// STEP 1: Add NDVI & NDBI
+var img = l5_1990;   // <-- THIS is where l5_1990 is used
+
+var ndvi = img.normalizedDifference(['SR_B4', 'SR_B3']).rename('NDVI');
+var ndbi = img.normalizedDifference(['SR_B6', 'SR_B5']).rename('NDBI');
+
+var stacked = img.addBands(ndvi).addBands(ndbi);
+
+// STEP 2: TRAINING DATA (You draw these in GEE manually)
+// urban, veg, water, soil must appear in the left panel after drawing
+var trainingFC = urban.merge(veg).merge(water).merge(soil);
+
+var training = stacked.sampleRegions({
+  collection: trainingFC,
+  properties: ['class'],
+  scale: 30,
+  tileScale: 4
+});
+
+// STEP 3: Train classifier
+var classifier = ee.Classifier.smileRandomForest(100)
+  .train({
+    features: training,
+    classProperty: 'class',
+    inputProperties: stacked.bandNames()
+  });
+
+// STEP 4: Classify
+var classified = stacked.classify(classifier);
+
+// STEP 5: Visualize
+var palette = [
+  'ff0000', // Urban
+  '00ff00', // Vegetation
+  '0000ff', // Water
+  'ffff00'  // Soil
+];
+
+Map.addLayer(classified, {min: 1, max: 4, palette: palette}, 'LULC 1990');
+
+// STEP 6: Calculate built-up area (class = 1)
+var urbanMask = classified.eq(1);
+var pixelArea = ee.Image.pixelArea().updateMask(urbanMask);
+
+var area = pixelArea.reduceRegion({
+  reducer: ee.Reducer.sum(),
+  geometry: hyderabad,
+  scale: 30,
+  maxPixels: 1e13
+});
+
+print('Urban area (sq meters): ', area.get('area'));
+print('Urban area (sq km): ', ee.Number(area.get('area')).divide(1e6));
+
+// STEP 7: Export
+Export.image.toDrive({
+  image: classified,
+  description: 'Hyderabad_LULC_1990',
+  scale: 30,
+  region: hyderabad,
+  maxPixels: 1e13
+});
+
